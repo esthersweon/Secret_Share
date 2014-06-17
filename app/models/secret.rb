80094946a5e21b7b016eedaaaba6050ad3bfb127
Esther S Weon <esther.s.weon@gmail.com>
2014-06-17 14:38:29 -0700

    Secret Taggings

--- app/models/secret.rb
+++ app/models/secret.rb
@@ -3,8 +3,8 @@ class Secret < ActiveRecord::Base
 
   belongs_to :author, :class_name => "User"
   belongs_to :recipient, :class_name => "User"
-  # has_many :secret_taggings, inverse_of: :secret
-  # has_many :tags,
-  #   through: :secret_taggings,
-  #   source: :tag
+  has_many :secret_taggings, inverse_of: :secret
+  has_many :tags,
+    through: :secret_taggings,
+    source: :tag
 end
